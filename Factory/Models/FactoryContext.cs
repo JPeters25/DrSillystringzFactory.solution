85792ed33d5c5f332e81a0b94984bacb04a99c7d
Justin Peters <JMarvel525@gmail.com>
2022-08-05 15:24:33 -0700

    WIP: add funcitonality to display machine and engineer on same splash page with clickable links

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -7,6 +7,7 @@ namespace Factory.Models
     public DbSet<Engineer> Engineers { get; set; }
     public DbSet<Machine> Machines { get; set; }
     public DbSet<EngineerMachine> EngineerMachine { get; set; }
+    
     public FactoryContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
