8b06256a3f4aaeb7ca84b5b6c1f34d7be8e44663
Justin Peters <JMarvel525@gmail.com>
2022-08-05 13:46:33 -0700

    add new column to machines table in database

---
+++ Factory/Migrations/20220805201238_serial number column to machines.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class serialnumbercolumntomachines : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                table: "Machines",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "Machines");
+        }
+    }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -14,6 +14,7 @@ namespace Factory.Models
     
     public int MachineId { get; set; }
     public string Name { get; set; }
+    public string SerialNumber { get; set;}
     public string Description { get; set; }
     public string Location { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
