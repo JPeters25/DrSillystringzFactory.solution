814f7ec2a23f0480d9fe3203477da2a03262af5e
Justin Peters <JMarvel525@gmail.com>
2022-08-05 13:03:33 -0700

    add html for Engineers files

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -7,11 +7,11 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-  public class EngineerController : Controller
+  public class EngineersController : Controller
   {
     private readonly FactoryContext _db;
 
-    public EngineerController(FactoryContext db)
+    public EngineersController(FactoryContext db)
     {
       _db = db;
     }
