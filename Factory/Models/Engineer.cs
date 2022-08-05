de5966d022008bb199ef546f18499f3dc98fa1cc
Justin Peters <JMarvel525@gmail.com>
2022-08-05 16:34:01 -0700

    add styling for Machines

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -13,8 +13,7 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     public string Name { get; set; }
     public string License { get; set; }
-    public int MachineId { get; set; 
-    }
+   
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
 
     }
